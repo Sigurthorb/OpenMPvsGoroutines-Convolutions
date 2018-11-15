ab7dd23dc80992468d09d486d70aa19164ee7068
ameyabp <ameyap@umd.edu>
2018-11-15 18:45:45 -0500

    Optimize C convolution - perform image bounds check for 0 padding once per channel

--- ConvolutionLibrary/c/libConv.c
+++ ConvolutionLibrary/c/libConv.c
@@ -5,35 +5,40 @@ void Convolution(unsigned char* input, unsigned char* output, int height, int wi
     int row, col, ch, krow, kcol;
     unsigned char ucValue = 0;
     double dValueKernel = 0;
-    double dAgg = 0;
+    double * dAgg = (double *)malloc(sizeof(double) * channels);
+    unsigned char ucAgg = 0;
 
     for(row = 0; row < height; row++)
     {
         for(col = 0; col < width; col++)
         {
-            for(ch = 0; ch < channels; ch++)
+            for (krow = 0; krow < kSize; krow++)
             {
-                dAgg = 0;
-                for (krow = 0; krow < kSize; krow++)
+                for (kcol = 0; kcol < kSize; kcol++)
                 {
-                    for (kcol = 0; kcol < kSize; kcol++)
+                    // zero padding for source image
+                    if ((row - kSize / 2 + krow) >= 0 && (row - kSize / 2 + krow) < height &&
+                        (col - kSize / 2 + kcol) >= 0 && (col - kSize / 2 + kcol) < width)
                     {
-                        // zero padding for source image
-                        if ((row - kSize / 2 + krow) >= 0 && (row - kSize / 2 + krow) < height &&
-                            (col - kSize / 2 + kcol) >= 0 && (col - kSize / 2 + kcol) < width)
+                        for(ch = 0; ch < channels; ch++)
                         {
                             ucValue = input[((row - kSize / 2 + krow) * width * channels) + ((col - kSize / 2 + kcol) * channels) + ch];
                             dValueKernel = kernel[krow * kSize + kcol];
-                            dAgg += ucValue * dValueKernel;
+                            dAgg[ch] += ucValue * dValueKernel;
                         }
                     }
                 }
-                unsigned char ucAgg = dAgg;
-                if (dAgg - ucAgg >= 0.500000)
+            }
+
+            for (ch = 0; ch < channels; ch++)
+            {
+                ucAgg = dAgg[ch];
+                if (dAgg[ch] - ucAgg >= 0.5)
                 {
                     ucAgg += 1;
                 }
                 output[(row * width * channels) + (col * channels) + ch] = ucAgg;
+                dAgg[ch] = 0;
             }
         }
     }
