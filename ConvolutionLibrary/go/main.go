8741df26420d1bedc3a639ccf83b0513ffe3eb14
Sigurthor Bjorgvinsson <sigurthor12@ru.is>
2018-11-27 18:42:48 -0500

    Go status checking with working padded array optimization

--- ConvolutionLibrary/go/main.go
+++ ConvolutionLibrary/go/main.go
@@ -7,57 +7,58 @@ import (
 	"unsafe"
 )
 
-type coord struct {
-	x int
-	y int
-}
-
 //export Convolution
 func Convolution(inputPtr *C.uchar, outputPtr *C.uchar, height, width, channels int, kernelPtr *C.float, kSize int) {
-	// OMP_NUM_THREADS
-	// GOMAXPROCS
-	var i int
-	routines := runtime.GOMAXPROCS(0)
-	//print(routines)
-
 	var wg sync.WaitGroup
-	kernelRowLen := kSize / 2
-	kernelColLen := kSize / 2
 
-	length := height * width * channels
-	step := width * channels
+	routines := runtime.GOMAXPROCS(0)
+	k := kSize / 2
+	padding := kSize - 1
 
-	input := (*[1 << 31]C.uchar)(unsafe.Pointer(inputPtr))[:length:length]
-	output := (*[1 << 31]C.uchar)(unsafe.Pointer(outputPtr))[:length:length]
+	inputStep := (width + padding) * channels
+	outputStep := width * channels
+
+	input := (*[1 << 31]C.uchar)(unsafe.Pointer(inputPtr))[: (height+padding)*inputStep : (height+padding)*inputStep]
+	output := (*[1 << 31]C.uchar)(unsafe.Pointer(outputPtr))[: height*outputStep : height*outputStep]
 	kernel := (*[1 << 31]C.float)(unsafe.Pointer(kernelPtr))[: kSize*kSize : kSize*kSize]
 
 	worker := func(id int) {
+		//fmt.Println("JUST JOINED THE PARTY -- ", id)
 		runtime.LockOSThread()
-		var startKRow, startKCol, maxKRowLen, maxKColLen, ai, aj, ac, r, c int
+		var startKRow, startKCol, endKRow, endKCol, ai, aj, ac, rInput, cInput, rOutput, cOutput, channelStartLoc int
 		var val C.float
 		var sum []C.float
 
-		for r = id; r < height; r += routines {
-			for c = 0; c < width; c++ {
+		rInput = id + k
+		rOutput = id
 
-				startKRow = -kernelRowLen + r
-				startKCol = -kernelColLen + c
+		for rInput < height+k {
+			cInput = k
+			cOutput = 0
+			for cInput < width+k {
 
-				maxKRowLen = kernelRowLen + r
-				maxKColLen = kernelColLen + c
+				startKRow = rInput - k
+				startKCol = cInput - k
 
-				sum = []C.float{0.0, 0.0, 0.0}
+				endKRow = rInput + k
+				endKCol = cInput + k
 
-				for ai = startKRow; ai <= maxKRowLen; ai++ {
-					for aj = startKCol; aj <= maxKColLen; aj++ {
+				sum = []C.float{0.0, 0.0, 0.0, 0.0}
+
+				for ai = startKRow; ai <= endKRow; ai++ {
+					for aj = startKCol; aj <= endKCol; aj++ {
+						channelStartLoc = ai*inputStep + aj*channels
 						for ac = 0; ac < channels; ac++ {
-							if ai < 0 || aj < 0 || ai >= height || aj >= width {
-								continue
-							}
-							sum[ac] += C.float(input[ai*step+aj*channels+ac]) * kernel[(ai-startKRow)*kSize+(aj-startKCol)]
+							//fmt.Println(rInput, cInput)
+							//fmt.Println("row:", rInput, ", col:", cInput, ", channel:", ac, ", indx:", channelStartLoc+ac, ", inputStep:", inputStep, ", height:", height+k) // ", d:", ", d:", (height+padding)*inputStep)
+							//fmt.Println()
+							var test = kernel[(ai-startKRow)*kSize+(aj-startKCol)]
+							sum[ac] += C.float(input[channelStartLoc+ac]) * test
 						}
 					}
 				}
+
+				channelStartLoc = rOutput*outputStep + cOutput*channels
 
 				for ac = 0; ac < channels; ac++ {
 					val = C.float(C.int(sum[ac]))
@@ -65,15 +66,25 @@ func Convolution(inputPtr *C.uchar, outputPtr *C.uchar, height, width, channels 
 						val++
 					}
 
-					output[r*step+c*channels+ac] = C.uchar(val)
+					output[channelStartLoc+ac] = C.uchar(val)
 				}
+
+				cInput++
+				cOutput++
+
 			}
+
+			//runtime.UnlockOSThread()
+			//wg.Done()
+			//return
+			rInput += routines
+			rOutput += routines
 		}
 		runtime.UnlockOSThread()
 		wg.Done()
 	}
 
-	for i = 0; i < routines-1; i++ {
+	for i := 0; i < routines-1; i++ {
 		wg.Add(1)
 		go worker(i)
 	}
