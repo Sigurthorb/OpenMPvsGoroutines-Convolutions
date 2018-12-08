91cf1fc2293817adb4244c0adf5391bc609d83d8
Sigurthor Bjorgvinsson <sigurthor12@ru.is>
2018-12-08 16:14:04 -0500

    Added collapse and dynamic scheduler with chunkSize width / numThreads; refactored channels to allow for better spatial locality

--- ConvolutionLibrary/c/libConv.c
+++ ConvolutionLibrary/c/libConv.c
@@ -3,52 +3,56 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <omp.h>
+#include <math.h>
 
 void Convolution(unsigned char* paddedInput, unsigned char* output, int height, int width, int channels, float* kernel, int kSize) {
     int row, col, ch, krow, kcol;
     unsigned char ucValue = 0;
     double dValueKernel = 0;
-    double dAgg = 0;
+    // TODO: Allocate dynamically for each thread so that we can take in any number of channels
+    double dAgg[] = {0.0, 0.0, 0.0, 0.0};
+    
     unsigned char ucAgg = 0;
-
-    pid_t processID = getpid();
+    int chunkSize = width;
+    int numThreads = 1;
+    int threadId = 0;
 
 #ifdef _OPENMP
-    int maxThreads = omp_get_max_threads();
-    int processorCount = omp_get_num_procs();
-    printf("%d: maxThreads %d, processorCount %d\n", processID, maxThreads, processorCount);
+    numThreads = omp_get_max_threads();
+    chunkSize = ceil((double)width/numThreads);
 #endif
+    int kHalf = kSize/2;
+    int paddedInputHeight = height + kHalf * 2;
+    int paddedInputWidth = width + kHalf * 2;
 
-    int paddedInputHeight = height + (kSize/2) * 2;
-    int paddedInputWidth = width + (kSize/2) * 2;
-    int log = 0;
-
-    // perform convolutions
-#pragma omp parallel private(col, ch, krow, kcol, ucValue, dValueKernel, dAgg, ucAgg, log)
-    for(row = kSize/2; row < paddedInputHeight - kSize/2; row++)
+#pragma omp parallel for private(col, row, ch, krow, kcol, ucValue, dValueKernel, dAgg, ucAgg) collapse(2) schedule(dynamic, chunkSize)
+    for(row = kHalf; row < paddedInputHeight - kHalf; row++)
     {
-#pragma omp parallel for private(col, ch, krow, kcol, ucValue, dValueKernel, dAgg, ucAgg, log)
-        for(col = kSize/2; col < paddedInputWidth - kSize/2; col++)
+        for(col = kHalf; col < paddedInputWidth - kHalf; col++)
         {
-            for (ch = 0; ch < channels; ch++)
+            for (krow = 0; krow < kSize; krow++)
             {
-                for (krow = 0; krow < kSize; krow++)
+                for(kcol = 0; kcol < kSize; kcol++)
                 {
-                    for(kcol = 0; kcol < kSize; kcol++)
+                    for (ch = 0; ch < channels; ch++)
                     {
-                        ucValue = paddedInput[((row - kSize / 2 + krow) * paddedInputWidth * channels) + ((col - kSize / 2 + kcol) * channels) + ch];
+                        ucValue = paddedInput[((row - kHalf + krow) * paddedInputWidth * channels) + ((col - kHalf + kcol) * channels) + ch];
                         dValueKernel = kernel[krow * kSize + kcol];
-                        dAgg += ucValue * dValueKernel;
+                        dAgg[ch] += ucValue * dValueKernel;
                     }
                 }
+            }
 
-                ucAgg = dAgg;
-                if (dAgg - ucAgg >= 0.5)
+            for (ch = 0; ch < channels; ch++)
+            {
+                ucAgg = dAgg[ch];
+                if (dAgg[ch] - ucAgg >= 0.5)
                 {
                     ucAgg += 1;
                 }
-                output[((row - kSize/2) * width * channels) + ((col - kSize/2) * channels) + ch] = ucAgg;
-                dAgg = 0;
+
+                output[((row - kHalf) * width * channels) + ((col - kHalf) * channels) + ch] = ucAgg;
+                dAgg[ch] = 0.0;
             }
         }
     }
